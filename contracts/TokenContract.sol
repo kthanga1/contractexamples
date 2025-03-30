376d0442b8428a8beb502c4954b1ef1048d64bc4
kthanga1 <kthanga1@asu.edu>
2025-03-30 19:14:23 +0000

    ERC721 code

---
+++ contracts/TokenContract.sol
@@ -0,0 +1,32 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//Code reference - https://github.com/OpenZeppelin/openzeppelin-contracts/blob/1873ecb38e0833fa3552f58e639eeeb134b82135/contracts/token/ERC721/ERC721.sol
+
+contract TokenContract is ERC721{
+    
+    event Token(address sender, string name);
+    string private _message;
+    constructor() ERC721("selfish-square", "SFFHFELHFA"){
+        _message = "Metaverse NFT identity solutions - kthanga1"; //"Metaverse NFT identity solutions - kthanga1";
+     } 
+
+    function message() public view   virtual returns (string memory) {
+        return _message;
+    }
+
+    function name() public view  override  virtual returns (string memory) {
+        return super.name();
+    }
+    function symbol() public view  override  virtual returns (string memory) {
+        return super.symbol();
+    }
+
+    function getTokenName() external {
+         emit Token( msg.sender , super.name());
+    }
+
+}
--- contracts/simple.js
+++
@@ -1,4 +0,0 @@
-import Web3 from "web3";;
-
-console.log(Web3.utils.padLeft(web3.utils.asciiToHex("Name"), 32));
-console.log(Web3.utils.asciiToHex("Name"))
